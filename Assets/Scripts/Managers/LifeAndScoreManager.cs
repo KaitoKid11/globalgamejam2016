8556f22393b7ffe1b2775c676e5ae0fea598ef7f
KaitoKid1111 <daleorei@gmail.com>
2016-01-30 09:21:42 +0100

    First Merge

--- Assets/Scripts/Managers/LifeAndScoreManager.cs
+++ Assets/Scripts/Managers/LifeAndScoreManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Life&ScoreManager : MonoBehaviour {
+public class LifeAndScoreManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
