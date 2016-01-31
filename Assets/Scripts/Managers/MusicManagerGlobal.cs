bb3ab6b8347a16a238a5da93e7767bc751372608
Daniel Ruiz Manero <drmane13@gmail.com>
2016-01-31 12:59:41 +0100

    Music.

---
+++ Assets/Scripts/Managers/MusicManagerGlobal.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManagerGlobal : Singleton<MusicManagerGlobal> {
+
+    public AudioClip [] _globalMusic;
+    public AudioClip [] _effects;
+
+    public int _currentMusic;
+
+    public int _selectedArray;
+    // 0 -> global, 1 -> _effects
+
+    public bool _random;
+
+    // Use this for initialization
+    void Start () {
+        _currentMusic = 0;
+        _selectedArray = 0;
+
+        int idMusic = 0;
+
+        if (_random)
+            idMusic = new System.Random().Next(0, _globalMusic.Length - 1);
+
+        playMusic(_currentMusic, idMusic);
+    }
+	
+    public void playMusic(int musicId, int selectedArray)
+    {
+        if(_selectedArray == 0)
+        {
+            GetComponent<AudioSource>().clip = _globalMusic[_currentMusic];
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void stopMusic()
+    {
+        GetComponent<AudioSource>().Stop();
+    } 
+}
